373572771676469dcad88be5ad1a43c989b372a6
James Muhoro <jmuhoro@innovexsolutions.co.ke>
2023-02-06 11:46:10 +0300

    Added secret key to next auth configuration

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 
 
 export default NextAuth({
+    secret: process.env.SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
